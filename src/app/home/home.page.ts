0b45f97d89368a00035df8433838364d016fcff2
danprogbr <programador.vff@gmail.com>
2024-03-31 13:01:09 -0300

    da teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {DataService} from "../services/data.service";
-import {LoadingController} from "@ionic/angular";
-import {Data} from "../services/nf.interface";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DataService } from "../services/data.service";
+import { LoadingController, ToastController } from "@ionic/angular";
+import { Data } from "../services/nf.interface";
+import { catchError } from 'rxjs/operators'; // Corrigido o import
 
 @Component({
   selector: 'app-home',
@@ -12,40 +13,51 @@ import {Data} from "../services/nf.interface";
 export class HomePage implements OnInit {
   form!: FormGroup; // Using definite assignment assertion
   isLoading = false;
-  data: any;
+  data: Data | undefined; // Definindo o tipo para a variável data
+  error = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private dataService: DataService,
-    private loadingController: LoadingController
-  ) {
-  }
+    private loadingController: LoadingController,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       cpfCnpj: [
         '',
-        [Validators.required, Validators.maxLength(2)],
+        [Validators.required], // Removido Validators.maxLength(2)
       ],
-      numeroPedido: ['', [Validators.required, Validators.maxLength(2)]],
+      numeroPedido: ['', [Validators.required]], // Removido Validators.maxLength(2)
     });
   }
 
   async pesquisar() {
     this.data = undefined;
     this.isLoading = true;
-    this.dataService.obterNf(`241`, ``).subscribe((res) => {
-        this.data = res;
-        this.isLoading = false;
-      }, error => {
-        this.isLoading = false;
-        console.error(error);
-      },
-      () => {
-        this.isLoading = false;
-      })
+    this.dataService.obterNf(this.form.get('numeroPedido')?.value, this.form.get('cpfCnpj')?.value)
+      .subscribe(
+        (res) => {
+          console.log(`retorno`, res);
+          this.data = res;
+          this.isLoading = false;
+        },
+        (error) => {
+          this.isLoading = false;
+          console.error(error);
+          this.showToast();
+        }
+      );
   }
 
-
-
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'Erro ao consultar NF, tente novamente mais tarde.',
+      duration: 2000, // Duration in milliseconds
+      position: 'bottom', // Position of the toast message
+      color: 'danger'
+    });
+    toast.present();
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {map, Observable, tap} from "rxjs";
+import {catchError, map, Observable, tap, throwError} from "rxjs";
 import {NotaFiscal, Data} from "../services/nf.interface"
 
 @Injectable({
@@ -17,19 +17,36 @@ export class DataService {
 
   public obterNf(pedido: string, cpfCnpj: string): Observable<Data> {
     const headers = new HttpHeaders({
-      'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json',
       'Accept': '*/*',
       'Accept-Encoding': 'gzip, deflate, br',
       'Connection': 'keep-alive'
     });
 
+    console.log(`chamada`);
+
     return this.httpService.post(
       `https://cutmixrastreio.netlify.app/.netlify/functions/consulta-detalhes-nf?numeroPedido=${pedido}&cpfCnpj=${cpfCnpj}`,
-      {}
+      {},
+      { headers: headers }
     ).pipe(
-      tap(() => console.log(`VV`)),
-      map((res: any) => res.data)
+      map((res: any) => res.data),
+      catchError(this.handleError)
     );
+  }
+
+  private handleError(error: any) {
+    let errorMessage = 'Ocorreu um erro ao processar sua solicitação.';
+
+    if (error.error instanceof ErrorEvent) {
+      // Erro do lado do cliente
+      errorMessage = `Erro: ${error.error.message}`;
+    } else {
+      // Erro do lado do servidor
+      errorMessage = `Código do erro: ${error.status}\nMensagem: ${error.message}`;
+    }
+
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
